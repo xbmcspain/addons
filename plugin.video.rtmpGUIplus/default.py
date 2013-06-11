9d41359beb1e6abb6234f42430355d68ab476de6
djd2 <dj_d2_sesion@hotmail.com>
2013-06-11 14:40:27 +0300

    Añadidos mas bases para futuros canales y secciones

--- plugin.video.rtmpGUIplus/default.py
+++ plugin.video.rtmpGUIplus/default.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import xbmc, xbmcgui, xbmcplugin, xbmcaddon
+from resources.lib.main import main
+
 __settings__   = xbmcaddon.Addon()
 
 BASE=[
-('http://playstationstorelibre.eshost.es/ps/Espana.xml', 'España'),
-('https://dl.dropbox.com/s/doa8gt9s0tdo2y7/Espana.xml', 'España Backup'),
+('http://playstationstorelibre.eshost.es/ps/Espana.xml', 'TV España'),
+('https://dl.dropbox.com/s/doa8gt9s0tdo2y7/Espana.xml', 'Respaldo TV España'),
+#('http://playstationstorelibre.eshost.es/ps/Series.xml', 'Series España'),
+]
+BASE1=[
+('http://playstationstorelibre.eshost.es/ps/Espana.xml', 'TV España'),
+('https://dl.dropbox.com/s/doa8gt9s0tdo2y7/Espana.xml', 'Respaldo TV España'),
+#('http://playstationstorelibre.eshost.es/ps/Series.xml', 'Series España'),
+#('http://playstationstorelibre.eshost.es/ps/Series.xml', 'TV Rumana'),
 ]
 BASE2=[
-('http://playstationstorelibre.eshost.es/ps/Espana.xml', 'España'),
-('https://dl.dropbox.com/s/doa8gt9s0tdo2y7/Espana.xml', 'España Backup'),
-('http://playstationstorelibre.eshost.es/ps/+18.xml', 'Adultos'),
+('http://playstationstorelibre.eshost.es/ps/Espana.xml', 'TV España'),
+('https://dl.dropbox.com/s/doa8gt9s0tdo2y7/Espana.xml', 'Respaldo TV España'),
+#('http://playstationstorelibre.eshost.es/ps/Series.xml', 'Series España'),
+('http://playstationstorelibre.eshost.es/ps/+18.xml', 'Canales Adultos'),
 ]
-if (__name__ == "__main__") and (__settings__.getSetting("habilitarmodoadultos") == 'true'): from resources.lib.main import main;main(BASE2)
+BASE3=[
+('http://playstationstorelibre.eshost.es/ps/Espana.xml', 'TV España'),
+('https://dl.dropbox.com/s/doa8gt9s0tdo2y7/Espana.xml', 'Respaldo TV España'),
+('http://playstationstorelibre.eshost.es/ps/Series.xml', 'Series España'),
+('http://playstationstorelibre.eshost.es/ps/Series.xml', 'TV Rumana'),
+('http://playstationstorelibre.eshost.es/ps/+18.xml', 'Canales Adultos'),
+]
+#if (__settings__.getSetting("habilitarmodoadultos") == 'true') and (__settings__.getSetting("canalesrumanos") == 'true'): from resources.lib.main import main;main(BASE3)
+if (__settings__.getSetting("habilitarmodoadultos") == 'true'): from resources.lib.main import main;main(BASE2)
+#if (__settings__.getSetting("canalesrumanos") == 'true'): from resources.lib.main import main;main(BASE1)
 else: from resources.lib.main import main;main(BASE)
